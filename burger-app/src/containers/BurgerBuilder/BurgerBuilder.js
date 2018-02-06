ceb97147b81113ffae5b91a49fada21d16b6a7f9
spaghetti-n5 <eleonora.baret@gmail.com>
2018-02-06 17:10:49 +0100

    Adding BuildControl component

--- burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,10 +6,10 @@ class BurgerBuilder extends Component {
 
   state = {
     ingredients: {
-      salad: 0,
-      bacon: 0,
-      cheese: 0,
-      meat: 0
+      salad: 2,
+      bacon: 2,
+      cheese: 1,
+      meat: 1
     }
   }
 
