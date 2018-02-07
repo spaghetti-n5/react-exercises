4675023125b932dc2d6c7e4250517b009d345359
spaghetti-n5 <eleonora.baret@gmail.com>
2018-02-07 14:22:42 +0100

    Display and update the price of the burger

--- burger-app/src/components/Burger/BuildControls/BuildControls.js
+++ burger-app/src/components/Burger/BuildControls/BuildControls.js
@@ -10,14 +10,15 @@ const controls = [
 ]
 const buildControls = (props) => (
   <div className={classes.BuildControls}>
-    {controls.map(ctrl => (
-      <BuildControl
-        key={ctrl.label}
-        label={ctrl.type}
-        added={()=> props.ingredientAdded(ctrl.type)}
-        removed={() => props.ingredientRemoved(ctrl.type)}
-        disabled={props.disabled[ctrl.type]}/>
-    ))}
+      <p>Current price: <strong>€ {props.price.toFixed(2)}</strong></p>
+      {controls.map(ctrl => (
+        <BuildControl
+          key={ctrl.label}
+          label={ctrl.type}
+          added={()=> props.ingredientAdded(ctrl.type)}
+          removed={() => props.ingredientRemoved(ctrl.type)}
+          disabled={props.disabled[ctrl.type]}/>
+      ))}
   </div>
 
 );
