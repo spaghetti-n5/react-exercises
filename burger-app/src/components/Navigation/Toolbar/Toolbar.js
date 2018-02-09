97738fe98df6c5550b786264dfe6d991892ad609
spaghetti-n5 <eleonora.baret@gmail.com>
2018-02-09 17:17:10 +0100

    Added reusable Navigation Items and fixed warning in Toolbar.js

--- burger-app/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-app/src/components/Navigation/Toolbar/Toolbar.js
@@ -2,15 +2,16 @@ import React from 'react';
 
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
-  <head className={classes.Toolbar}>
+  <header className={classes.Toolbar}>
     <div>MENU</div>
     <Logo/>
     <nav>
-      ...
+      <NavigationItems/>
     </nav>
-  </head>
+  </header>
 );
 
 export default toolbar;
