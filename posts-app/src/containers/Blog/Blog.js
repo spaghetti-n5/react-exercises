91cb8397025b20bb337750c2e8abdae201e019b6
spaghetti-n5 <eleonora.baret@gmail.com>
2018-02-19 10:55:25 +0100

    Handling errors locally

--- posts-app/src/containers/Blog/Blog.js
+++ posts-app/src/containers/Blog/Blog.js
@@ -9,12 +9,13 @@ import './Blog.css';
 class Blog extends Component {
   state = {
     posts: [],
-    selectedPostId: null
+    selectedPostId: null,
+    error: false
   }
 
   componentDidMount () {
     axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then (response => {
+        .then(response => {
           const posts = response.data.slice(0,3);
           const updatedPosts = posts.map(post => {
             return {
@@ -22,8 +23,13 @@ class Blog extends Component {
               author: 'John'
             }
           });
+          //console.log(response);
           this.setState({posts: updatedPosts})
         })
+        .catch(error => {
+          //console.log(error);
+          this.setState({error: true});
+        });
   }
 
   postSelectedHandler = (id) => {
@@ -31,13 +37,16 @@ class Blog extends Component {
   }
 
     render () {
-      const posts = this.state.posts.map(post => {
-        return <Post
-          key={post.id}
-          title={post.title}
-          author={post.author}
-          clicked={()=> this.postSelectedHandler(post.id)}/>
-      })
+      let posts = <p style={{textAlign: "center"}}>Something went wrong.</p>
+      if (!this.state.error) {
+        posts = this.state.posts.map(post => {
+          return <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={()=> this.postSelectedHandler(post.id)}/>
+        });
+      }
 
         return (
             <div>
