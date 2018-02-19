980110afe1559b07cb2ce06b60cdc0c413d9ec9a
spaghetti-n5 <eleonora.baret@gmail.com>
2018-02-19 13:07:00 +0100

    Sending a POST request within purchaseContinueHandler method

--- burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+
+import axios from '../../axios-orders'; //importing the instance of axios
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -77,7 +80,24 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('You continue');
+    //alert('You continue');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      costumer: {
+          name: 'Peter Tomson',
+          address: {
+              street: 'Teststreet 34',
+              zipCode: '17755',
+              Country: 'NYC'
+          },
+          email: 'test@test.com'
+      },
+      deliveryMethod: 'fastest'
+    }
+    axios.post('/orders.json', order) //Endpoint to POST with Firebase
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
   }
 
   render () {
