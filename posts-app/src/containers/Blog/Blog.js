7a1fca445127a41db4ec6aa4f3f5379fe1faec1e
spaghetti-n5 <eleonora.baret@gmail.com>
2018-02-20 18:07:29 +0100

    Styling the active route with NavLink

--- posts-app/src/containers/Blog/Blog.js
+++ posts-app/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -12,12 +12,12 @@ class Blog extends Component {
               <header className="Blog">
                 <nav>
                   <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to={{
+                    <li><NavLink to="/" exact>Home</NavLink></li>
+                    <li><NavLink to={{
                           pathname: '/new-post',
                           hash: '#submit',
                           search: '?quick-submit=true'
-                        }}>New Post</Link></li>
+                        }}>New Post</NavLink></li>
                   </ul>
                 </nav>
               </header>
