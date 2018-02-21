5789aadbf62c073240c478c8f191a0b82fcbf9f1
spaghetti-n5 <eleonora.baret@gmail.com>
2018-02-21 13:52:57 +0100

    Nested Routes added

--- posts-app/src/containers/Blog/Blog.js
+++ posts-app/src/containers/Blog/Blog.js
@@ -4,16 +4,21 @@ import { Route, NavLink, Switch } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
     render () {
         return (
-          <div>
-              <header className="Blog">
+          <div  className="Blog">
+              <header>
                 <nav>
                   <ul>
-                    <li><NavLink to="/" exact>Home</NavLink></li>
+                    <li><NavLink
+                          to="/posts/" exact
+                          activeClassName="my-active"
+                          activeStyle={{
+                                  color: '#fa923f',
+                                  textDecoration: 'underline'
+                          }}>Home</NavLink></li>
                     <li><NavLink to={{
                           pathname: '/new-post',
                           hash: '#submit',
@@ -25,9 +30,8 @@ class Blog extends Component {
               {/*<Route path="/" exact render={() => <h1>Home1</h1>}/>
               <Route path="/" render={() => <h1>Home2</h1>}/>*/}
               <Switch>
-                <Route path="/" exact component={Posts}/>
                 <Route path="/new-post" component={NewPost}/>
-                <Route path="/:id" exact component={FullPost}/>
+                <Route path="/posts" component={Posts}/>
               </Switch>
           </div>
         );
