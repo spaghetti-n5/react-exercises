b520dd5dc6bb5d4e38d33f1b04cc29e1af804b51
spaghetti-n5 <eleonora.baret@gmail.com>
2018-02-26 10:53:20 +0100

    404 page added

--- routing-exercise/src/App.js
+++ routing-exercise/src/App.js
@@ -5,6 +5,7 @@ import { Route, NavLink, Switch } from 'react-router-dom';
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
 //import Course from './containers/Course/Course';
+import NoMatch from './components/NoMatch/NoMatch';
 
 class App extends Component {
   render () {
@@ -35,6 +36,8 @@ class App extends Component {
             <Route path="/users" component={Users}/>
             {/*<Route path="/courses/:courseId/:courseTitle" component={Course}/>*/}
             <Route path="/courses" component={Courses}/>
+            <Route component={NoMatch}/>
+            {/*<Route render={()=><h1>Not Found</h1>}/>*/}
           </Switch>
         </div>
       //</BrowserRouter>
