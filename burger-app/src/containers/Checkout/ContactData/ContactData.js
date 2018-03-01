ffef9d1403ca6f7c6633ff68c60d211a7a56c655
spaghetti-n5 <eleonora.baret@gmail.com>
2018-03-01 14:31:27 +0100

    Handling user input

--- burger-app/src/containers/Checkout/ContactData/ContactData.js
+++ burger-app/src/containers/Checkout/ContactData/ContactData.js
@@ -82,6 +82,21 @@ class ContactData extends Component {
         });
   }
 
+  inputChangedHandler = (event, inputIdentifier) => {
+    //console.log(event.target.value);
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    };
+    //console.log(updatedOrderForm);
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier]
+    };
+    //console.log(updatedFormElement);
+    updatedFormElement.value = event.target.value;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    this.setState({orderForm:updatedOrderForm});
+  }
+
   render() {
     const formElementsArray = [];
     for (let key in this.state.orderForm) {
@@ -99,7 +114,7 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
-          />
+            changed={(event)=> this.inputChangedHandler(event,formElement.id)}/>
         ))}
         <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
       </form>
