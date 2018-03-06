287e90a5ec4040aee83128ccccbe918857e673bc
spaghetti-n5 <eleonora.baret@gmail.com>
2018-03-06 17:07:56 +0100

    Route added

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import './App.css';
 import LocationSearch from './containers/LocationSearch/LocationSearch';
+import CurrentWeather from './containers/CurrentWeather/CurrentWeather';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LocationSearch/>
+        <Route path="/" exact component={LocationSearch} />
+        <Route path="/weather" component={CurrentWeather} />
       </div>
     );
   }
