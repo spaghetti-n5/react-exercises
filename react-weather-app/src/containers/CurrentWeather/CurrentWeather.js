4eaaa74f6f8af7088372b907a18dacd4c595a78f
spaghetti-n5 <eleonora.baret@gmail.com>
2018-03-07 11:59:44 +0100

    Handling current weather API calls

--- react-weather-app/src/containers/CurrentWeather/CurrentWeather.js
+++ react-weather-app/src/containers/CurrentWeather/CurrentWeather.js
@@ -1,22 +1,47 @@
 import React, { Component } from 'react';
 
+import axios from 'axios';
+
 class CurrentWeather extends Component {
   state = {
-    inputValue: ''
+    inputValue: '',
+    weather: '',
+    temperature: 0,
+    humidity: 0
   }
 
   componentDidMount () {
     //Extracting the inputValue of the user from the query params
     const query = new URLSearchParams(this.props.location.search);
+    let city = "";
     for (let param of query.entries()) {
-      console.log(param);
+      //console.log(param);
       this.setState({inputValue: param[1]});
+      city = param[1];
+      //console.log(this.props);
     }
+
+    //Current weather API Request. The first weather condition in API respond is primary.
+    axios.get('https://api.openweathermap.org/data/2.5/weather?id=524901&APPID=fdb4c75cbd6ca2deef4b9b0db49ace4a&q=' + city)
+        .then(response => {
+          //console.log(response.data);
+          this.setState({weather:response.data.weather[0].main,
+                         temperature:response.data.main.temp,
+                         humidity: response.data.main.humidity})
+        })
+        .catch(error => {
+          console.log(error);
+        });
   }
 
   render () {
     return (
-      <p>Weather for: {this.state.inputValue}</p>
+      <div>
+        <p>Current weather for: {this.state.inputValue}</p>
+        <p>{this.state.weather}</p>
+        <p>Temperature in F{this.state.temperature}</p>
+        <p>Humidity {this.state.temperature}</p>
+      </div>
     );
   }
 }
