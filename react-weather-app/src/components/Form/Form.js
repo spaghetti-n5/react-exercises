d8dee8dd9a9c9feaf9f8023fb29869f0e18f1e4c
spaghetti-n5 <eleonora.baret@gmail.com>
2018-03-19 12:33:39 +0100

    Updating CSS and Form component

--- react-weather-app/src/components/Form/Form.js
+++ react-weather-app/src/components/Form/Form.js
@@ -4,7 +4,6 @@ import classes from './Form.css';
 
 const form = (props) => (
   <form className={classes.Form} onSubmit={props.submitted}>
-    <h2>Enter a Location</h2>
     <input type="text" onChange={props.changed} value={props.value} placeholder="City name"/>
     <input className={classes.Button} type="submit" value="Submit" />
   </form>
