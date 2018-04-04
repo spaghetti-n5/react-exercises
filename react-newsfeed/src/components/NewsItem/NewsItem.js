3fa2c45a229059d84fe4dfef96e8f44d454bdf9d
spaghetti-n5 <eleonora.baret@gmail.com>
2018-04-04 12:53:20 +0200

    Adding CSS style to NewsItem

--- react-newsfeed/src/components/NewsItem/NewsItem.js
+++ react-newsfeed/src/components/NewsItem/NewsItem.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
+import classes from './NewsItem.css';
+
 const NewsItem = (props) => (
   <article>
-    <div>
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-      <p>{props.author} | {props.source}</p>
-    </div>
     <img src={props.image} alt={props.title} />
+    <div className={classes.Text}>
+       <h3>{props.title}</h3>
+       <p>{props.description}</p>
+       <p style={{fontStyle: 'italic'}}>{props.author} | {props.source}</p>
+    </div>
   </article>
 );
 
