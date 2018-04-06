10db453a75e763d776280dfb1abaa85890a23de3
spaghetti-n5 <eleonora.baret@gmail.com>
2018-04-06 14:05:56 +0200

    Adding default image for articles without url and improving CSS style

--- react-newsfeed/src/components/NewsItem/NewsItem.js
+++ react-newsfeed/src/components/NewsItem/NewsItem.js
@@ -1,16 +1,27 @@
 import React from 'react';
 
 import classes from './NewsItem.css';
+import defaultImage from './defaultImage.jpg';
 
-const NewsItem = (props) => (
-  <article>
-    <img src={props.image} alt={props.title} />
-    <div className={classes.Text}>
-       <h3>{props.title}</h3>
-       <p>{props.description}</p>
-       <p style={{fontStyle: 'italic'}}>{props.author} | {props.source}</p>
-    </div>
-  </article>
-);
+const NewsItem = (props) => {
+  let image = '';
+  if (props.image === null) {
+    image = <img src={defaultImage} alt='default' />
+  } else {
+    image = <img src={props.image} alt={props.title} />
+  }
+  return (
+    <article>
+      <div>
+         {image}
+      </div>
+      <div className={classes.Text}>
+         <h3>{props.title}</h3>
+         <p>{props.description}</p>
+         <p style={{fontStyle: 'italic'}}>{props.author} | {props.source}</p>
+      </div>
+    </article>
+  );
+}
 
 export default NewsItem;
